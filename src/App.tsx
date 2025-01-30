12ef2a6c6a0b240061bd941caa70cd4d52069538
MladenKovacic <mladen.kovacic406@gmail.com>
2025-01-30 11:24:28 +0100

    more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Pokedex from "./components/Pokedex";
-import Pokecard  from "./components/Pokecard";
-
-
+import Pokecard from "./components/Pokecard";
+import Player from "./components/Player";
 
 function App() {
-
   return (
     <>
       <h1>Pokedex</h1>
-      <Pokecard id={4} name="Charmander" type="fire" exp={62}/>
+      {/* <Pokecard id={4} name="Charmander" type="fire" exp={62}/> */}
+      <Player />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.tsx
@@ -0,0 +1,14 @@
+// import Pokecard from "./Pokecard";
+interface Playerprops {
+  id: number;
+  hand: [];
+}
+
+export default function Player({ id, hand }: Playerprops): JSX.Element {
+  return (
+    <>
+      <h3>player {id}</h3>
+      <div className="cards">{hand.map((hands) => hands)}</div>
+    </>
+  );
+}
