57621eaa5cd894e8d9a2d487da331ab13f7a6c6f
sara-jpallander <sara.j.pallander@gmail.com>
2025-01-30 13:26:03 +0100

    bla

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,16 @@
-// import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
+import Pokecard from "./Pokecard";
+
+
 interface Playerprops {
   id: number;
   hand: [];
 }
 
 export default function Player({ id, hand }: Playerprops): JSX.Element {
+
+  
+
   return (
     <>
       <h3>player {id}</h3>
